7acef2673fb9d5515edd7cf0c5477acb069b23f2
Fernando Augusto Sampaio Bonafe Fernandes <05640913754@receita.intranet>
2021-05-07 20:43:13 -0300

    update

---
+++ src/web/sw.js
@@ -0,0 +1,32 @@
+const filesToCache = [
+'/bibliotecas/jspanel/jspanel.min.js',
+'/bibliotecas/jspanel/jspanel.min.css',
+'/bibliotecas/tabulator/css/tabulator.min.css',
+'/bibliotecas/tabulator/css/tabulator.scss',
+'/bibliotecas/tabulator/js/tabulator.min.js',
+'/bibliotecas/visjs/vis-network.min.js',
+'/bibliotecas/visjs/vis.timeline-graph2d.min.js',
+'/bibliotecas/humanize-duration.js',
+'/modulos/atividades/atividades_view.js',
+'/modulos/atividades/atividades_dao.js',
+'/modulos/cpf/CPF.js',
+'/modulos/pessoas/usuario_dao.js',
+'/modulos/processos_trabalho/base/2021_05_02_processos_trabalho_rfb.js',
+'/modulos/processos_trabalho/competencias_view.js',
+'/modulos/processos_trabalho/processos_trabalho_dao.js',
+'/modulos/processos_trabalho/processos_trabalho_view.js',
+'/TrabalhoRFB.html',
+'/trabalho_rfb.js'
+];
+
+const staticCacheName = 'pages-cache-v1';
+
+self.addEventListener('install', event => {
+  console.log('Attempting to install service worker and cache static assets');
+  event.waitUntil(
+    caches.open(staticCacheName)
+    .then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
