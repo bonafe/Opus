3222eeba861f0dc0a41c7008f3e428183dc9b5a0
Fernando Augusto Sampaio Bonafe Fernandes/RF08/SRF <bonafe@opapagaiorinoceronte.net>
2021-05-21 17:29:36 -0300

    update

--- src/python/ServidorHTTPS.py
+++ src/python/ServidorHTTPS.py
@@ -8,10 +8,12 @@ import os
 web_dir = os.path.join(os.path.dirname(__file__), '../web')
 os.chdir(web_dir)
 
-server_address = ('localhost', 4443)
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain('../python/cert.pem')
+#context.load_cert_chain('../python/opus.alfvcp.rf08.srf.pem')
+
+server_address = ('opus.alfvcp.rf08.srf', 443)
 httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket(httpd.socket,
-                               server_side=True,
-                               certfile='../python/cert.pem',
-                               ssl_version=ssl.PROTOCOL_TLS)
+httpd.socket = context.wrap_socket(httpd.socket,
+                               server_side=True)
 httpd.serve_forever()
